bdebe632eca4c7e78061deefa1a16589fc76c24e
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-01-25 18:17:23 -0500

    Prep the repository for mono-repo blueprints
    * setup travis for auto-publishing on tag detection.
    * bump version

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,16 @@
 app_path="packages/@ember/octane-app-blueprint"
 addon_path="packages/@ember/octane-app-blueprint"
 
+# https://github.com/npm/npm/issues/7042#issuecomment-186300168
+
+echo "//registry.npmjs.org/:_password=${NPM_PASSWORD}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_USERNAME}" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_EMAIL}" >> ~/.npmrc
+
 # Publish App Blueprint
+cp README.md $app_path/README.md
 ( cd $app_path && yarn build && npm publish )
+
 
 # Publish Addon Blueprint
