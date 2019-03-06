03ffa8cf1290f2158bcfcba419bbcd998f5d28a9
Robert Jackson <me@rwjblue.com>
2019-03-05 20:05:43 -0500

    Avoid GitHub rate limiting issues. (#62)
    * Avoid GitHub rate limiting issues.
    
    Attempts to grab the latest ember-cli version via an unauthenticated
    v3 REST API request first (much smaller / faster / better), **BUT** if
    we hit a 403 error (due to rate limiting) fall back to fetching the
    tarball (this request is _not_ rate limited) and getting the SHA from
    that directly.
    
    * Ensure that octane-blueprint-utils is using the local version.
    
    * Regenerate yarn.lock for all packages.

--- packages/octane-blueprint-utils/index.js
+++ packages/octane-blueprint-utils/index.js
@@ -2,15 +2,59 @@
 
 const got = require('got');
 
-function getRepoCommitVersion(owner, repo, branch = 'master') {
+function getEmberCLIVersionFromTarball() {
+  // lazy to avoid extra work when not rate-limited
+  const tar = require('tar-stream');
+  const zlib = require('zlib');
+
+  return new Promise((resolve, reject) => {
+    let version;
+    let extract = tar.extract();
+    extract.on('entry', function(header, stream, next) {
+      let isRoot = header.name.indexOf('/') === header.name.length - 1;
+      if (isRoot) {
+        version = header.name.split('-')[4].slice(0, -1);
+      }
+
+      stream.resume();
+      stream.on('end', next);
+    });
+
+    got
+      .stream('http://github.com/ember-cli/ember-cli/tarball/master')
+      .pipe(zlib.createGunzip())
+      .pipe(extract)
+      .on('error', (reason) => {
+        reject(reason);
+      })
+      .on('finish', () => {
+        if (version === undefined) {
+          reject(new Error('tarball was malformed'));
+        } else {
+          resolve(version);
+        }
+      });
+  });
+}
+
+function getEmberCLIVersion() {
   return got(
-    `https://api.github.com/repos/${owner}/${repo}/commits/${branch}`,
-    {
-      headers: {Accept: 'application/vnd.github.VERSION.sha'},
-    },
-  ).then(result => `github:${owner}/${repo}#${result.body}`);
+    `https://api.github.com/repos/ember-cli/ember-cli/git/refs/heads/master`,
+    { json: true },
+  )
+    .then(result => {
+      return result.body.object.sha;
+    })
+    .catch(result => {
+      if (result.statusCode === 403 && result.headers['x-ratelimit-remaining'] === '0') {
+        return getEmberCLIVersionFromTarball();
+      }
+
+      throw error;
+    })
+  .then(version => `github:ember-cli/ember-cli#${version}`);
 }
 
 module.exports = {
-  getRepoCommitVersion: getRepoCommitVersion,
+  getEmberCLIVersion
 };
