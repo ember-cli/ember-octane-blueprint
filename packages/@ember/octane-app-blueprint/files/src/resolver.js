6ae9e18f9bcfd5fee760d6a298caea726e9afb09
Pepe Cano <825430+ppcano@users.noreply.github.com>
2019-03-08 09:29:41 +0100

    Do not add custom types to the default resolver

--- packages/@ember/octane-app-blueprint/files/src/resolver.js
+++ packages/@ember/octane-app-blueprint/files/src/resolver.js
@@ -3,26 +3,15 @@ import buildResolverConfig from 'ember-resolver/ember-config';
 import config from '../config/environment';
 
 let moduleConfig = buildResolverConfig(config.modulePrefix);
+
 /*
  * If your application has custom types and collections, modify moduleConfig here
- * to add support for them.
+ * to add support for them:
+ *
+ *  moduleConfig.types = Object.assign(moduleConfig.types, {
+ *    'validator': { definitiveCollection: 'validators' },
+ *  });
  */
-
-
- moduleConfig.types = Object.assign(moduleConfig.types, {
-   // TODO: why did emberclear add this to main collection?
-   //       the existing config
-   //       https://github.com/ember-cli/ember-resolver/blob/master/mu-trees/addon/ember-config.js#L17
-   //       specifies the config 'definitiveCollection'
-  'config': { definitiveCollection: 'main' },
-  // TODO: add these to the default ember-config in ember-resolver
-  'util': { definitiveCollection: 'utils' },
-  // TODO: remove when addons can add collections and types themselves
-  //       and when sparkles-component specifies this.
-  //       OR, remove when glimmer components land
-  'component-manager': { definitiveCollection: 'main' },
- });
-
 export default Resolver.extend({
   config: moduleConfig
 });
