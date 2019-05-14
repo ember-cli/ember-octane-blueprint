1ae14bcb1fd7fafea56b64ece0e290baf5b1bebd
L. Preston Sego III <LPSego3+dev@gmail.com>
2019-05-14 09:16:42 -0400

    remove flags that are enabled by default (#106)

--- packages/@ember/octane-addon-blueprint/files/tests/dummy/config/environment.js
+++ packages/@ember/octane-addon-blueprint/files/tests/dummy/config/environment.js
@@ -9,12 +9,8 @@ module.exports = function(environment) {
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
-        EMBER_NATIVE_DECORATOR_SUPPORT: true,
-        EMBER_METAL_TRACKED_PROPERTIES: true,
-        EMBER_GLIMMER_ANGLE_BRACKET_NESTED_LOOKUP: true,
-        EMBER_GLIMMER_ANGLE_BRACKET_BUILT_INS: true,
-        EMBER_GLIMMER_FN_HELPER: true
+        // e.g. EMBER_MODULE_UNIFICATION: true
+        EMBER_METAL_TRACKED_PROPERTIES: true
       },
       EXTEND_PROTOTYPES: {
         // Prevent Ember Data from overriding Date.parse.
