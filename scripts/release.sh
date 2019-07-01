20f4414ad5d602ecdc913fb0c04120592bedfe58
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-07-01 18:17:25 -0400

    automate more of the release process

---
+++ scripts/release.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# bumps the version of the blueprint packages
+#
+# $1 - major | minor | patch
+#      (really just whatever you can pass to `npm version`)
+
+# exit if any command fails
+set -e
+
+versionIncrement=$1
+packages=( "packages/@ember/octane-app-blueprint" "packages/@ember/octane-addon-blueprint" )
+
+for var in "${packages[@]}"
+do
+  echo "Updating version of ${var}..."
+  (
+      cd $var && \
+        npm version  ${versionIncrement}
+  )
+done
+
+firstPackage=${packages[0]}
+nextVersion=$(cat $firstPackage/package.json | jq -r ".version")
+
+# TODO: also update changelog?
+
+echo "Comitting bump to $nextVersion"
+
+git commit -am"Bump version to $nextVersion"
+
+echo "Tagging v$nextVersion"
+
+git tag "v$nextVersion"
+
+git push origin master --tags
