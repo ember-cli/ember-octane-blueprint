57d229b64fd8a6b20e682026611a2a38151106d9
Godfrey Chan <godfreykfc@gmail.com>
2019-08-19 18:56:29 -0700

    Add missing comma

--- packages/@ember/octane-app-blueprint/files/.eslintrc.js
+++ packages/@ember/octane-app-blueprint/files/.eslintrc.js
@@ -49,7 +49,7 @@ module.exports = {
         // this can be removed once the following is fixed
         // https://github.com/mysticatea/eslint-plugin-node/issues/77
         'node/no-unpublished-require': 'off'
-      })
+      }),
       extends: [
         'plugin:node/recommended'
       ],
